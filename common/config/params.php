aacfdcc16296d3ba9a33178331440817f332256b
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-12 23:14:06 +0100

    Preparing to Bootstrap4 usage

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,9 @@
 <?php
 return [
-    'adminEmail' => 'akiraz@bk.ru',
-    'supportEmail' => 'akiraz@bk.ru',
+    'adminEmail' => 'example@mail.com',
+    'supportEmail' => 'examplesupport@mail.com',
     'user.passwordResetTokenExpire' => 3600,
     'frontendHost' => 'http://localhost:8100/',
+    'bsVersion' => '4.x',
+    'bsDependencyEnabled' => false,
 ];
