6b438809cc4244a774bf474f035637a4f413c459
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-13 02:08:01 +0100

    Basic landing page layout

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,50 +2,64 @@
 
 /* @var $this yii\web\View */
 
+use kartik\tabs\TabsX;
+
 $this->title = 'My Yii Application';
+
+$tabIconSize = '1.5em;';
+
+$tabItems = [
+    [
+        'label' => '<span style="font-size: ' . $tabIconSize . '"><i class="fas fa-receipt pull-left"></i></span> Expense',
+        'content' => 'The expenses tab',
+        'active' => true,
+    ],
+    [
+        'label' => '<span style="font-size: ' . $tabIconSize . '"><i class="fas fa-exchange-alt pull-left"></i></span> Transfer',
+        'content' => 'The transfer tab',
+        'active' => false,
+    ],
+    [
+        'label' => '<span style="font-size: ' . $tabIconSize . '"><i class="fas fa-hand-holding-usd pull-left"></i></span> Income',
+        'content' => 'This is the income tab',
+        'active' => false,
+    ],
+];
 ?>
+
 <div class="site-index">
-
-    <div class="card jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
-
+        <div class="row mb-2">
+            <div class="col-md-12">
+                <?= TabsX::widget(
+                    [
+                        'items' => $tabItems,
+                        'align' => TabsX::ALIGN_CENTER,
+                        'encodeLabels' => false,
+                        'bordered' => true,
+                        'height' => TabsX::SIZE_SMALL,
+                    ]
+                ) ?>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">Quick overview widgets</div>
+                        <div class="card-text">A placeholder for the charts and expense alerts</div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">Transactions</div>
+                        <div class="card-text">A placeholder for the transaction list</div>
+                    </div>
+                </div>
             </div>
         </div>
 
