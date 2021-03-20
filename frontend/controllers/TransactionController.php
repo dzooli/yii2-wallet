b9e3eafdba26b3fa5bb47adf5b9f4cd5787deadf
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-03-20 14:43:39 +0100

    Basic CRUD with access control

---
+++ frontend/controllers/TransactionController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Transaction;
+use frontend\models\TransactionSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\filters\HttpCache;
+
+/**
+ * TransactionController implements the CRUD actions for Transaction model.
+ */
+class TransactionController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+            // 'cacheControl' => [
+            //     'class' => HttpCache::class,
+            //     'only' => ['index'],
+            //     'cacheControlHeader' => 'public, max-age=0',
+            // ],
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['index', 'create', 'update', 'delete', 'view'],
+                'rules' => [
+                    [
+                        'allow' => false,
+                        'actions' => ['index', 'create', 'update', 'delete', 'view'],
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'actions' => ['index', 'create', 'update', 'delete', 'view'],
+                        'roles' => ['@'],
+                    ]
+                ],
+                'denyCallback' => function ($rule, $action) {
+                    return $this->redirect(['user/login', 'redirectTo' => $action->controller->id . '/index']);
+                }
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Transaction models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new TransactionSearch;
+        $dataProvider = $searchModel->search(Yii::$app->request->getQueryParams());
+
+        return $this->render('index', [
+            'trDataProvider' => $dataProvider,
+            'trSearchModel' => $searchModel,
+        ]);
+    }
+
+    /**
+     * Displays a single Transaction model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('view', ['model' => $model]);
+        }
+    }
+
+    /**
+     * Creates a new Transaction model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Transaction;
+        $model->created_at = strftime('%Y-%m-%d');
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Transaction model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Transaction model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Transaction model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Transaction the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Transaction::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
