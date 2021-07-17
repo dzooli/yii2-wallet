0351fd3c7734db5a97da6325801a4b1de7a65ae3
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-07-17 17:35:01 +0200

    default account creation on login

--- common/models/User.php
+++ common/models/User.php
@@ -2,9 +2,13 @@
 
 namespace common\models;
 
+use Yii;
+
+use common\models\AccountType;
+use common\models\Account;
+
+use common\exceptions\AccountCreationErrorException;
 use dektrium\user\models\User as BaseUser;
-use Exception;
-use yii\helpers\ArrayHelper;
 
 /**
  * Class User
@@ -26,12 +30,33 @@ class User extends BaseUser
     public function checkDefaultAccounts(): bool
     {
         $accTypes = $this->getAccountTypes();
-        return (in_array('Credit', $accTypes)) && in_array('Cash', $accTypes);
+        return (in_array('Outside', $accTypes)) && in_array('Cash', $accTypes);
     }
 
     public function createDefaultAccounts()
     {
-        // throw new Exception('Test account creation called.');
+        $accounts = [];
+        for ($i = 0; $i < 2; $i++) {
+            $accounts[] = new Account([
+                'user_id' => $this->id,
+                'icon_id' => 1, 'color_id' => 1,
+                'balance' => 0.0,
+                'default_currency' => 1,
+            ]);
+        }
+        $accounts[0]->name = 'My pocket';
+        $accounts[0]->account_type_id = AccountType::TYPE_CASH;
+        $accounts[1]->name = 'Outside';
+        $accounts[1]->account_type_id = AccountType::TYPE_OUTSIDE;
+
+        foreach ($accounts as $acc) {
+            if (!$acc->save()) {
+                $errors = json_encode($acc->getErrors());
+                Yii::error($errors, 'wallet');
+                throw new AccountCreationErrorException($errors);
+            }
+        }
+        $this->refresh();
     }
 
     public function getAccounts()
